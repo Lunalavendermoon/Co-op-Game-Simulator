14939a431168c6814be7ac07d7558dc0fb946ca5
summerxa <anne.xia6@gmail.com>
2025-04-14 16:45:24 -0400

    Fix dialogue stuff

--- Assets/Scripts/Dialogue/TextMessageCommand.cs
+++ Assets/Scripts/Dialogue/TextMessageCommand.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Yarn.Unity;
 
 public class TextMessageCommand : MonoBehaviour
 {
     public GameObject messageBubblePrefab;
-    public Transform messageContainer;
+    public RectTransform messageContainer;
+    public ScrollRect scrollRect;
+
+    // the height of a single text message
+    public float messageHeight;
+    float betweenMessages;
+
+    void Start()
+    {
+        betweenMessages = GetComponent<VerticalLayoutGroup>().spacing;
+    }
 
     public void SpawnMessage(string sender, string message)
     {
         var bubble = Instantiate(messageBubblePrefab, messageContainer);
         bubble.GetComponent<TextMessageBubble>().SetMessage(sender, message);
+
+        
+        // Resizing the container extends it in both directions, so we must reposition it accordingly
+        Vector2 newPos = new Vector2(messageContainer.localPosition.x,
+                                        messageContainer.localPosition.y + messageHeight/2 + betweenMessages);
+
+        messageContainer.sizeDelta = new Vector2(messageContainer.sizeDelta.x, messageContainer.sizeDelta.y + messageHeight);
+        messageContainer.localPosition = newPos;
+
+        scrollRect.normalizedPosition = new Vector2(0, 0);
     }
 }
 

a25a67ec46e651320a4f8d2fd6edc6f919218f52
summerxa <anne.xia6@gmail.com>
2025-04-18 22:58:02 -0400

    Testing custom usernames

--- Assets/Scripts/Dialogue/TextMessageCommand.cs
+++ Assets/Scripts/Dialogue/TextMessageCommand.cs
@@ -6,28 +6,39 @@ using Yarn.Unity;
 
 public class TextMessageCommand : MonoBehaviour
 {
+    public DialogueRunner dialogueRunner;
+
     public GameObject messageBubblePrefab;
     public RectTransform messageContainer;
     public ScrollRect scrollRect;
+    public GameObject usernamePrompt;
+    public TMP_InputField usernameInput;
+    public GameObject usernameInvalidText;
+
+    string playerName = "You";
 
     float betweenMessages;
 
     void Start()
     {
         betweenMessages = messageContainer.GetComponent<VerticalLayoutGroup>().spacing;
+
+        if (usernameInvalidText != null) {
+            usernameInvalidText.SetActive(false);
+        }
     }
 
     public void SpawnMessage(string sender, string message)
     {
         var bubble = Instantiate(messageBubblePrefab, messageContainer);
-        bubble.GetComponent<TextMessageBubble>().SetMessage(sender, message);
+        bubble.GetComponent<TextMessageBubble>().SetMessage(sender.Equals("You") ? playerName : sender, message);
 
         ResizeContainer(bubble);
     }
 
     public void SpawnImageMessage(string sender, string message, string filename) {
         var bubble = Instantiate(messageBubblePrefab, messageContainer);
-        bubble.GetComponent<TextMessageBubble>().SetMessage(sender, message);
+        bubble.GetComponent<TextMessageBubble>().SetMessage(sender.Equals("You") ? playerName : sender, message);
 
         bubble.GetComponent<TextMessageBubble>().SetImage(filename);
         
@@ -47,6 +58,23 @@ public class TextMessageCommand : MonoBehaviour
         messageContainer.localPosition = newPos;
 
         scrollRect.normalizedPosition = new Vector2(0, 0);
+    }
+
+    public void SetPlayerName() {
+        // TODO check for swears
+        if (usernameInput.text.Contains("bad word")) {
+            SetUsernameInvalidText("No bad words allowed >:(");
+        } else if (usernameInput.text.Length == 0) {
+            SetUsernameInvalidText("Username can't be empty >:(");
+        } else {
+            playerName = usernameInput.text;
+            dialogueRunner.StartDialogue("Start");
+        }
+    }
+
+    void SetUsernameInvalidText(string warning) {
+        usernameInvalidText.GetComponent<TMP_Text>().text = warning;
+        usernameInvalidText.SetActive(true);
     }
 }
 

699df5e6c2787e3d4678c00bf407e5df9a823881
Lunalavendermoon <lunalavendermoon@gmail.com>
2025-04-20 11:41:07 -0700

    added dmg to da game :D

--- Assets/Scripts/Shooter/Bullet.cs
+++ Assets/Scripts/Shooter/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
-
+    public int damage = 1;
     public string type = "";
 
     void Update()
--- Assets/Scripts/Shooter/Enemy.cs
+++ Assets/Scripts/Shooter/Enemy.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 2f;
+    public int maxHealth = 3;
+    private int currentHealth;
     private PlayerController player;
 
     void Start()
     {
+        currentHealth = maxHealth;
         player = GameObject.FindWithTag("Player")?.GetComponent<PlayerController>();
     }
 
@@ -15,6 +18,14 @@ public class Enemy : MonoBehaviour
         transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
 
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") || other.CompareTag("Obstacle"))
@@ -28,8 +39,12 @@ public class Enemy : MonoBehaviour
 
         if (other.CompareTag("Bullet"))
         {
-            Destroy(gameObject);
-            Destroy(other.gameObject);
+            Bullet bullet = other.GetComponent<Bullet>();
+            if (bullet != null)
+            {
+                TakeDamage(bullet.damage);
+                Destroy(other.gameObject);
+            }
         }
     }
 

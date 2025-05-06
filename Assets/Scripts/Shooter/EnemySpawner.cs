74bef9ca5830e6e260879f8b4b79c291c754b939
Lunalavendermoon <lunalavendermoon@gmail.com>
2025-05-05 23:41:58 -0700

    bug fix

--- Assets/Scripts/Shooter/EnemySpawner.cs
+++ Assets/Scripts/Shooter/EnemySpawner.cs
@@ -3,43 +3,48 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public static EnemySpawner Instance;
     public GameObject enemyPrefab;
     public float spawnInterval = 1f;
     public float xBounds = 2f;
 
     private Coroutine spawnCoroutine;
 
-    void Start()
-    {
-        spawnCoroutine = StartCoroutine(SpawnLoop());
+    void Awake() {
+        if (Instance != null && Instance != this) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+        }
     }
 
-    IEnumerator SpawnLoop()
-    {
-        while (true)
-        {
+    void OnEnable() {
+        if (Instance == this) {
+            spawnCoroutine = StartCoroutine(SpawnLoop());
+        }
+    }
+
+    IEnumerator SpawnLoop() {
+        yield return new WaitForSeconds(spawnInterval); // optional: delay first spawn
+        while (true) {
             SpawnEnemy();
             yield return new WaitForSeconds(spawnInterval);
         }
     }
 
-    void SpawnEnemy()
-    {
+    void SpawnEnemy() {
         float randomX = Random.Range(-xBounds, xBounds);
         Vector3 spawnPos = new Vector3(transform.position.x + randomX, transform.position.y, 0);
         Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
     }
 
-    // Call this method to update the interval
-    public void UpdateSpawnInterval(float newInterval)
-    {
+    public void UpdateSpawnInterval(float newInterval) {
         Debug.Log($"Updating spawn interval to {newInterval}");
         spawnInterval = newInterval;
 
-        if (spawnCoroutine != null)
-        {
+        if (spawnCoroutine != null) {
             StopCoroutine(spawnCoroutine);
-            spawnCoroutine = StartCoroutine(SpawnLoop()); // restart loop with new interval
+            spawnCoroutine = StartCoroutine(SpawnLoop());
         }
     }
 }
